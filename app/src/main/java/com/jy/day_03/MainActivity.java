c69eaff2f2ceffb680d1cad013cb3ceaaf87bc03
xiaochaoren <1124598738@qq.com>
2021-06-24 00:47:58 +0800

    桌面删除注释

--- app/src/main/java/com/jy/day_03/MainActivity.java
+++ app/src/main/java/com/jy/day_03/MainActivity.java
@@ -30,7 +30,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
- * 添加注释
+ *  桌面删除注释
  */
 public class MainActivity extends AppCompatActivity {
 
