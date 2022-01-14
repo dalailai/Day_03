a2a4518c8d7cfdcd8a3b9aa2e3a64ae1a0128f2f
mac_dalailai <1124598738@qq.com>
2022-01-14 09:32:27 +0800

    mac_commit 2

--- app/src/main/java/com/jy/day_03/MainActivity.java
+++ app/src/main/java/com/jy/day_03/MainActivity.java
@@ -53,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+
         initView();
         initData();
 
