a6ac906d0a93afed233590ec98ea307e0ccabdb3
mac_dalailai <1124598738@qq.com>
2022-01-14 09:19:31 +0800

    mac_commit

--- app/src/main/java/com/jy/day_03/MainActivity.java
+++ app/src/main/java/com/jy/day_03/MainActivity.java
@@ -107,6 +107,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
         isVpScroll = false;
 
         mVp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
